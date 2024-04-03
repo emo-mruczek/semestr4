ea47d13fb9291f992a206b3af6b4b79f735dbce4
Felix <krokcia1@gmail.com>
2024-04-03 22:20:48 +0200

    == itd operators

--- jpp/3/1/GF.hpp
+++ jpp/3/1/GF.hpp
@@ -7,16 +7,103 @@ class GF {
 
     unsigned int value;
 
-    unsigned int convert(unsigned int n) {
-        return n % size;
-    }
-
     public:
 
+    // konstruktor
     GF() {
         value = 0;
     }
 
+    GF(unsigned int n ) {
+        value = n % size;
+    }
+
+    // operatory porównań
+    // ==
+    friend bool operator==(const GF& obj_a, const GF& obj_b) {
+        return (obj_a.value == obj_b.value) ? true : false;
+    }
+
+    friend bool operator==(const GF& obj_a, unsigned int n) {
+        return (obj_a.value == n % obj_a.size) ? true : false;
+    }
+
+    friend bool operator==(unsigned int n, const GF& obj_a) {
+        return (obj_a.value == n % obj_a.size) ? true : false;
+    }
+
+    // !=
+     friend bool operator!=(const GF& obj_a, const GF& obj_b) {
+        return (obj_a.value != obj_b.value) ? true : false;
+    }
+
+    friend bool operator!=(const GF& obj_a, unsigned int n) {
+        return (obj_a.value != n % obj_a.size) ? true : false;
+    }
+
+    friend bool operator!=(unsigned int n, const GF& obj_a) {
+        return (obj_a.value != n % obj_a.size) ? true : false;
+    }
+
+    // <=
+    friend bool operator<=(const GF& obj_a, const GF& obj_b) {
+        return (obj_a.value <= obj_b.value) ? true : false;
+    }
+
+    friend bool operator<=(const GF& obj_a, unsigned int n) {
+        return (obj_a.value <= n % obj_a.size) ? true : false;
+    }
+
+    friend bool operator<=(unsigned int n, const GF& obj_a) {
+        return (n % obj_a.size <= obj_a.value  ) ? true : false;
+    }
+
+    // >=
+    friend bool operator>=(const GF& obj_a, const GF& obj_b) {
+        return (obj_a.value >= obj_b.value) ? true : false;
+    }
+
+    friend bool operator>=(const GF& obj_a, unsigned int n) {
+        return (obj_a.value >= n % obj_a.size) ? true : false;
+    }
+
+    friend bool operator>=(unsigned int n, const GF& obj_a) {
+        return (n % obj_a.size >= obj_a.value ) ? true : false;
+    }
+
+     // <
+    friend bool operator<(const GF& obj_a, const GF& obj_b) {
+        return (obj_a.value < obj_b.value) ? true : false;
+    }
+
+    friend bool operator<(const GF& obj_a, unsigned int n) {
+        return (obj_a.value < n % obj_a.size) ? true : false;
+    }
+
+    friend bool operator<(unsigned int n, const GF& obj_a) {
+       return (n % obj_a.size < obj_a.value ) ? true : false;
+    }
+
+    // >
+    friend bool operator>(const GF& obj_a, const GF& obj_b) {
+        return (obj_a.value > obj_b.value) ? true : false;
+    }
+
+    friend bool operator>(const GF& obj_a, unsigned int n) {
+        return (obj_a.value > n % obj_a.size) ? true : false;
+    }
+
+    friend bool operator>(unsigned int n, const GF& obj_a) {
+        return (n % obj_a.size > obj_a.value ) ? true : false;
+    }
+
+    // operatory arytmetyczne
+    
+
+
+
+    // operatory podstawień
+    // przypisanie
     GF& operator=(const GF& obj) {
         if (this != &obj) {
             this->value = obj.value;
@@ -25,10 +112,11 @@ class GF {
     }
 
     GF& operator=(unsigned int n) {
-        this->value = this->convert(n);
+        this->value = n % size;
         return *this;
     }
 
+    // streamy
     friend std::ostream& operator<<(std::ostream& os, GF obj) {
         return os << obj.value;
     }
--- jpp/3/1/test.cpp
+++ jpp/3/1/test.cpp
@@ -11,13 +11,78 @@ int main() {
     GF a;
     GF b;
 
-    cout << "Ich obecne wartości to: a - " << a << " b - " << b << endl;
+    cout << "Ich obecne wartości to: a: " << a << " b: " << b << endl;
 
     a = 22;
-    cout << "Podstawiam za a wartosc 22: a - " << a << endl;
+    cout << "Podstawiam za a wartosc 22: a: " << a << endl;
 
     b = a;
-    cout << "Podstawiam b do a: b - " << b << endl;
+    cout << "Podstawiam a do b: b: " << b << endl;
+
+    cout << "Czy a == b? ";
+    cout << ((a == b) ? "prawda\n" : "fałsz\n");
+
+    cout << "Czy a == 1? ";
+    cout << ((a == 1) ? "prawda\n" : "fałsz\n");
+
+    cout << "Czy a == 22? ";
+    cout << ((a == 22) ? "prawda\n" : "fałsz\n");
+
+    cout << "Czy a == 3? ";
+    cout << ((a == 3) ? "prawda\n" : "fałsz\n");
+
+    cout << "Czy 1 == a? ";
+    cout << ((1 == a) ? "prawda\n" : "fałsz\n");
+
+    cout << "Czy 3 == a? ";
+    cout << ((3 == a) ? "prawda\n" : "fałsz\n");
+
+    cout << "-------" << endl;
+
+    cout << "Czy a != b? ";
+    cout << ((a != b) ? "prawda\n" : "fałsz\n");
+
+    cout << "Czy a != 1? ";
+    cout << ((a != 1) ? "prawda\n" : "fałsz\n");
+
+    cout << "Czy a != 22? ";
+    cout << ((a != 22) ? "prawda\n" : "fałsz\n");
+
+    cout << "Czy a != 3? ";
+    cout << ((a != 3) ? "prawda\n" : "fałsz\n");
+
+    cout << "Czy 1 != a? ";
+    cout << ((1 != a) ? "prawda\n" : "fałsz\n");
+
+    cout << "Czy 3 != a? ";
+    cout << ((3 != a) ? "prawda\n" : "fałsz\n");
+
+    cout << "-------" << endl;
+
+    GF c = 12;
+    GF d = 0;
+
+    cout << "Tworzę c: " << c << " i d: " << d << endl;
+
+     cout << "Czy a <= c? ";
+    cout << ((a <= c) ? "prawda\n" : "fałsz\n");
+
+    cout << "Czy a > c? ";
+    cout << ((a > c) ? "prawda\n" : "fałsz\n");
+
+    cout << "Czy a < 12? ";
+    cout << ((a < 22) ? "prawda\n" : "fałsz\n");
+
+    cout << "Czy a < d? ";
+    cout << ((a < d) ? "prawda\n" : "fałsz\n");
+
+    cout << "Czy 0 < a? ";
+    cout << ((0 < a) ? "prawda\n" : "fałsz\n");
+
+    cout << "Czy d < a? ";
+    cout << ((d < a) ? "prawda\n" : "fałsz\n");
+
+
 
 
 }
