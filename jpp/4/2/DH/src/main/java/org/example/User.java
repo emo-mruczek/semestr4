eda80ebb468a9ac6c2e464c5e129254309b26322
emo-mruczek <110098604+emo-mruczek@users.noreply.github.com>
2024-04-17 23:19:26 +0200

    java: it works somehow

--- jpp/4/2/DH/src/main/java/org/example/User.java
+++ jpp/4/2/DH/src/main/java/org/example/User.java
@@ -1,4 +1,50 @@
 package org.example;
 
-public class User {
+import java.util.Random;
+
+public class User<T extends GF> {
+
+    private DHSetup<T> dh;
+    private long secret;
+    private T key;
+
+    private void setSecret() {
+        Random random = new Random();
+        long number = random.nextLong(100 - 10) + 10;
+        this.secret = number;
+        System.out.println("Mój sekret: " + secret);
+    }
+
+    public User(DHSetup<T> set) {
+        this.dh = set;
+        setSecret();
+    }
+
+    public T getPublicKey() {
+        T ret = dh.power(dh.getGenerator(), secret);
+        return ret;
+    }
+
+    public void setKey(T a) {
+        this.key = dh.power(a, secret);
+        System.out.println("Mój klucz: " + key);
+    }
+
+    public T encrypt(T m) {
+        if (key == null) {
+            System.out.println("You cannot encrypt before setting the key!");
+            return null;
+        } else {
+            return (T) m.mult(key);
+        }
+    }
+
+    public T decrypt(T c) {
+        if (key == null) {
+            System.out.println("You cannot decrypt before setting the key!");
+            return null;
+        } else {
+            return (T) c.div(key);
+        }
+    }
 }
--- jpp/4/2/DH/src/test/java/Test.java
+++ jpp/4/2/DH/src/test/java/Test.java
@@ -1,5 +1,6 @@
 import org.example.DHSetup;
 import org.example.GF;
+import org.example.User;
 
 public class Test {
   public static void main(String[] args) {
@@ -8,6 +9,33 @@ public class Test {
     System.out.println("Wspolna liczba pierwsza: " + dum.getCharacteristic());
     System.out.println("Wspolny generator: " + dh.getGenerator());
 
+    // tworze user Alice, konstruktor od razu tworzy sekret
+    User<GF> Alice = new User<>(dh);
+
+    // to samo Bob
+    User<GF> Bob = new User<>(dh);
+
+    // Alice liczy klucz publiczny
+    GF fromAlice = Alice.getPublicKey();
+    System.out.println("Klucz od Alice: " + fromAlice);
+
+    // przekazuje do Boba
+    Bob.setKey(fromAlice);
+
+    // Bob liczy klucz publiczny
+    GF fromBob = Bob.getPublicKey();
+    System.out.println("Klucz od Boba: " + fromBob);
+
+    // przekazuje do Alice
+    Alice.setKey(fromBob);
+
+    // Alice koduje wiadomosc
+    GF encrypted = Alice.encrypt(new GF(2137));
+    System.out.println("Zakodowana wiadomosc: " + encrypted);
+
+    // Bob dekoduje
+    GF decrypted = Bob.decrypt(encrypted);
+    System.out.println("Odkodowana wiadomosc: " + decrypted);
 
   }
 }
