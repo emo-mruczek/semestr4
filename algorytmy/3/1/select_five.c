4264cc7a35796b1675109393174357dbafb0d099
emo-mruczek <krokcia1@gmail.com>
2024-05-03 15:57:12 +0200

    eych

--- algorytmy/3/1/select_five.c
+++ algorytmy/3/1/select_five.c
@@ -45,7 +45,77 @@ void exchange(int* a, int* b) {
     *b = temp;
 }
 
+void insertion_sort(int arr[], int low, int high) {
+    for (int i = low + 1; i <= high; ++i) {
+        int key = arr[i];
+        int j = i - 1;
+        while (j >= low && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+}
 
+int partition_diff(int A[], int p, int r, int x) {
+    int i = p - 1;
+    for (int j = p; j < r; j++) {
+        if (A[j] == x){
+            i = j;
+            break;
+        }
+    }
+
+    if (i != p - 1) {
+        exchange(&A[r-1], &A[i]);
+    }
+
+    int store_index = p;
+
+    for (int j = p; j < r - 1; j++) {
+        if (A[j] < x){
+            exchange(&A[j], &A[store_index]);
+            store_index++;
+        }
+    }
+
+    exchange(&A[r-1], &A[store_index]);
+
+    return store_index;
+}
+
+int select_algorithm(int A[], int p, int r, int ind) {
+    int n = r - p + 1;
+    if (ind > 0 && ind <= n) {
+        if (n <= 5) {
+            insertion_sort(A, p, r);
+            return A[p + ind - 1];
+        }
+
+        // Divide A[] into groups of 5 elements
+        int number_of_groups = (n + 4) / 5;
+        int medians[number_of_groups];
+
+        for (int i = 0; i < number_of_groups; i++) {
+            int median_index = p + 5 * i + 2;
+            medians[i] = select_algorithm(A, p + 5 * i, median_index - 1, 3);
+        }
+
+        int median_of_medians = select_algorithm(medians, 0, number_of_groups - 1, number_of_groups / 2);
+
+        int index = partition_diff(A, p, r, median_of_medians);
+        int m = index - p + 1;
+
+        if (ind < m) {
+            return select_algorithm(A, p, index - 1, ind);
+        } else if (ind > m) {
+            return select_algorithm(A, index + 1, r, ind - m);
+        } else {
+            return A[index];
+        }
+    }
+    return -1; // Out of range index
+}
 
 int partition(int A[], int lo, int hi) {
     int pivot = A[lo];
@@ -81,137 +151,6 @@ void sort(int A[], int lo, int hi ) {
     }
  }
 
-int partition_diff(int A[], int p, int r, int x) {
-    int i = -1;
-    for (int j = p; j < r; j++) {
-        if (A[j] == x){
-            i = j;
-            break;
-        }
-    }
-
-    if (i != -1) {
-        exchange(&A[r-1], &A[i]);
-    }
-
-    int store_index = p;
-
-    for (int j = p; j < r - 1; j++) {
-        if (A[j] < x){
-            int t = A[j];
-            A[j] = A[store_index];
-            A[store_index] = t;
-            store_index++;
-        }
-    }
-
-    if (i != -1) {
-        exchange(&A[r-1], &A[store_index]);
-    }
-
-    return store_index;
-}
-
-
-
-int select_algorithm(int A[], int p, int r, int ind, int length) {
-
-     // podział na grupy 
-     int n = r - p + 1;
-     int number_of_groups = n / 5;
-
-     if ( n % 5 != 0) {
-         number_of_groups++;
-     }
-
-    printf("Liczba grup: %d\n", number_of_groups);
-
-     int tab[number_of_groups][5];
-     printf("wielkosc: %d\n", length);
-
-     int k = 1;
-     for (int i = 0; i < number_of_groups; i++) {
-
-         //printf("Numer grupy: %d\n", i);
-
-         for (int j = 0; j < 5; j++) {
-
-             if (k > length) {
-                 break;
-             }
-
-             tab[i][j] = A[k];
-             k++;
-             //printf("%d ", tab[i][j]);
-         }
-         //printf("\n");
-     }
-
-    // szukam mediany
-    k = 1;
-    int medians[number_of_groups];
-    for (int i = 0; i < number_of_groups; i++) {
-
-        int to_sort[5];
-        printf("Numer grupy: %d. W grupie: ", i);
-
-        for (int j = 0; j < 5; j++) {
-            if (k > length) {
-                //k--;
-                break;
-            }
-
-            to_sort[j] = tab[i][j];
-            printf("%d ", to_sort[j]);
-            k++;
-        }
-
-        int size_of_a_group;
-        if ((k-1)%5 == 0) {
-            size_of_a_group = 5;
-        } else {
-            size_of_a_group = (k-1) % 5;
-        }
-        printf("Wielosc grupy: %d\n", size_of_a_group);
-
-        sort(to_sort, 0, size_of_a_group - 1);
-
-        for (int x = 0; x < size_of_a_group; x++) {
-            printf("%d ", to_sort[x]);
-        }
-
-        int median = to_sort[size_of_a_group/2];
-        printf("mediana: %d \n", median);
-        medians[i] = median;
-    }
-
-    for (int i = 0; i < number_of_groups; i++) {
-        printf("%d ", medians[i]);
-    }
-
-    printf("\n");
-
-    // mediana median 
-    int median_of_medians;
-    if (number_of_groups == 1) {
-        median_of_medians = medians[0];
-    } else { 
-        median_of_medians = select_algorithm(medians, 0, number_of_groups - 1, (number_of_groups - 2) / 2, number_of_groups );
-    }
-    
-    int index = partition_diff(A, p, r, median_of_medians);
-    int m = index - p + 1;
-
-    printf("Index: %d \n", index);
-
-    if (ind < m) {
-        return select_algorithm(A, p, index - 1, ind, ind - index + 1);
-    } else if (ind > m) {
-        return select_algorithm(A, index + 1, r, ind - m , r - index - 1);
-    } else {
-        return A[index];
-    }
-}
 
 bool is_ok(int A[], int stat, int value) {
     if (A[stat] == value) {
@@ -238,10 +177,10 @@ int main() {
 
     printf("Podawaj wartosci tablicy: \n");
 
-    int A[length+1];
-    int Init[length+1];
+    int A[length];
+    int Init[length];
 
-    for (int i = 1; i < length+1; i++) {
+    for (int i = 0; i < length; i++) {
         int num;
         scanf("%d", &num);
         A[i] = num;
@@ -257,8 +196,15 @@ int main() {
         printf("Kluczowe momenty:\n");
     }
 
+    int pass;
+    if (stat == length) {
+        pass = 1;
+    } else {
+        pass = stat + 1;
+    }
+
     //właściwy algorytm
-    int value = select_algorithm(A, 1, length, stat, length); //czy jest git??
+    int value = select_algorithm(A, 0, length - 1, pass); //czy jest git??
 
     if (should_print) {
         printf("Tablica poczatkowa:\n");
