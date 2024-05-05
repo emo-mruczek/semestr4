11b5a5922c7b9ed0aa311042ca22638d9085648c
emo-mruczek <krokcia1@gmail.com>
2024-05-05 08:36:30 +0200

    bezpieka 4: it works probably

--- bezpieka/4/RSA.c
+++ bezpieka/4/RSA.c
@@ -2,69 +2,71 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <math.h>
-#include <stdint.h>
+//#include <stdint.h>
 #include <sys/random.h>
 #include <time.h>
+#include <inttypes.h>
 
 struct key_pair {
-    uint16_t n;
-    uint16_t key;
+    uint32_t n;
+    uint32_t key;
 };
 
-uint16_t prime1, prime2;
+uint32_t prime1, prime2;
 
-bool check_prime(uint16_t n) {
-    if (n <= 1 ) {
-        return false;
-    }
+bool check_prime(uint32_t n) {
+    uint32_t root = sqrt(n);
 
-    uint16_t i, j;
-    j = sqrt(n);
-
-    for (i = 2; i <= j; i++) {
-        if (n % i == 0) {
+    for(int i = 2; i <= root; i++) {
+        if(n % i == 0) {
             return false;
         }
     }
+
     return true;
 }
 
-uint16_t find_e(uint16_t euler) {
-    uint16_t seed;
+uint32_t find_e(uint32_t euler) {
+    uint32_t seed;
     getrandom(&seed, sizeof(seed), 0);
     srandom(seed);
-    uint16_t e;
+    uint32_t e;
 
-    while(true) {    
-        e = (1 + random() % (euler));
-
-        if(euler % e == 0) {
-            continue;
-        } 
-
+    do {    
+        e = (2 + random() % (euler));
         if (check_prime(e) && e != prime1 && e != prime2) {
             return e;
         }
-    }
+    } while(euler % e == 0);
 }
 
-uint16_t euclide(uint16_t n, uint16_t euler) {
-    uint16_t temp;
-    for (uint16_t i = 1; i < euler; i++) {
-        if ((i*n) % euler == 1) {
-            temp = 1;
-            break;
-        }
+uint32_t euclide(uint32_t a, uint32_t b) {
+    int16_t s0 = 1, s1 = 0, t0 = 0, t1 = 1, r0 = a, r1 = b;
+
+    while (r1 != 0) {
+        int16_t q = r0 / r1;
+
+        int16_t temp = r1;
+        r1 = r0 - q * r1;
+        r0 = temp;
+
+        temp = s1;
+        s1 = s0 - q * s1;
+        s0 = temp;
+
+        temp = t1;
+        t1 = t0 - q * t1;
+        t0 = temp;
     }
 
-    return temp;
+    return (s0 < 0) ? s0 + b : s0;
 }
 
 void calculate_keys(struct key_pair *priv, struct key_pair *pub) {
-    uint16_t n = prime1 * prime2;
-    uint16_t euler  = (prime1 - 1) * (prime2 - 1);
-    uint16_t e = find_e(euler);
-    uint16_t d = euclide(e, euler);
+    uint32_t n = prime1 * prime2;
+    uint32_t euler  = (prime1 - 1) * (prime2 - 1);
+    uint32_t e = find_e(euler);
+    uint32_t d = euclide(e, euler);
     priv->n = n;
     priv->key = d;
     pub->n = n;
@@ -73,17 +75,20 @@ void calculate_keys(struct key_pair *priv, struct key_pair *pub) {
 
 int main() {
     printf("First prime number: \n");
-    scanf("%hu", &prime1);
+    scanf("%" PRIu32, &prime1);
+    printf("Entered: %" PRIu32 "\n", prime1);
 
     printf("Second prime number: \n");
-    scanf("%hu", &prime2);
+    scanf("%" PRIu32, &prime2);
+    printf("Entered: %" PRIu32 "\n", prime2);
+
 
     if (!check_prime(prime1) || !check_prime(prime2) || prime1 == prime2) {
         printf("Numbers are not prime or are the same!");
         return 0;
     }
 
-    printf("gitgut wszystko jest pierwsze :3");
+    printf("gitgut wszystko jest pierwsze :3\n");
 
     struct key_pair priv, pub;
     calculate_keys(&priv, &pub);
@@ -91,8 +96,8 @@ int main() {
     //printing
 
     printf("Your keys: {key}, {n} \n");
-    printf("Public: %hu, %hu\n", pub.key, pub.n);
-    printf("Private: %hu, %hu\n", priv.key, priv.n);
+    printf("Public: %d, %d\n", pub.key, pub.n);
+    printf("Private: %d, %d\n", priv.key, priv.n);
 
 
     return 0;
