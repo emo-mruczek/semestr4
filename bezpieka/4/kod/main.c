e978dab7ed70636753c685e4f29e1d9f75433c4c
emo-mruczek <krokcia1@gmail.com>
2024-05-07 08:05:41 +0200

    bezpieka: further destroying the code

--- bezpieka/4/kod/main.c
+++ bezpieka/4/kod/main.c
@@ -5,9 +5,9 @@
 #include <stdbool.h>
 
 // from geeksforgeeks
-uint32_t power(uint32_t a, uint32_t b, uint32_t n){
+uint64_t power(uint64_t a, uint64_t b, uint64_t n){
 	  // Initialize answer
-    uint32_t res = 1;
+    uint64_t res = 1;
  
     // Check till the number becomes zero
     while (b > 0) {
@@ -26,7 +26,7 @@ uint32_t power(uint32_t a, uint32_t b, uint32_t n){
 }
 
 // from geeksforgeeks (again)
-uint32_t gcdExtended(uint32_t a, uint32_t b, uint32_t *x, uint32_t *y) {
+uint64_t gcdExtended(uint64_t a, uint64_t b, uint64_t *x, uint64_t *y) {
 
  if (a == 0) {
         *x = 0;
@@ -34,35 +34,35 @@ uint32_t gcdExtended(uint32_t a, uint32_t b, uint32_t *x, uint32_t *y) {
         return b;
     }
  
-    int x1, y1; // To store results of recursive call
-    int gcd = gcdExtended(b % a, a, &x1, &y1);
+    uint64_t x1, y1; // To store results of recursive call
+    uint64_t gcd = gcdExtended(b % a, a, &x1, &y1);
  
     // Update x and y using results of recursive
     // call
     *x = y1 - (b / a) * x1;
     *y = x1;
  
-    return gcd;}
+    return gcd;
+}
 
 
-uint32_t find_priv(struct key_pair priv_a, struct key_pair pub_a, struct key_pair pub_b) {
+uint64_t find_priv(struct key_pair priv_a, struct key_pair pub_a, struct key_pair pub_b) {
 	uint64_t kphi = priv_a.key * pub_a.key - 1;
-	printf("%" PRIu32 "\n", kphi);
+	printf("%" PRIu64 "\n", kphi);
 	uint64_t n = priv_a.n;
 	uint64_t t = kphi;
-	while(t % 2 == 0) {
+	while(t % 2 == 0) {  // nie wchodzi nigdy tutaj
 		t = t/2;
+        printf("t: %" PRIu64 "\n", t);
 	}
-	printf("%d" PRIu32 "\n", t);
 	uint64_t a = 2;
-	uint32_t k, x, r, p, q, temp1, temp2;
+	uint64_t k, x, r, p, q, temp1, temp2;
 	while(true) {
 		k = t;
-
-		while(k < kphi) {
+        //printf("k: %" PRIu64 "kphi: %" PRIu64 "/n", k, kphi );
+		while(k < kphi) { //tu tez nie, bo zawsze k = kphi, czemu?
 			x = power(a, k, n);
-
-//					printf("dupa\n");
+            
 			if(x != 1 && x != (n-1) && power(x, 2, n) == 1) {
 					p = gcdExtended(x-1, n, &temp1, &temp2);
 					break;
@@ -73,15 +73,15 @@ uint32_t find_priv(struct key_pair priv_a, struct key_pair pub_a, struct key_pai
 					}
 		q = n / p;
 		return q;
-					}
+}
 
 
 
 
 
 int main() {
-    uint32_t prime1 = 1234567891;
-    uint32_t prime2 = 1234577;
+    uint64_t prime1 = 1234567891;
+    uint64_t prime2 = 1234577;
 
     struct key_pair pub_a;
     struct key_pair pub_b;
@@ -93,18 +93,18 @@ int main() {
 
     
     printf("Your keys A: {key}, {n} \n");
-    printf("Public: %" PRIu32 ", %" PRIu32 "\n", pub_a.key, pub_a.n);
-    printf("Private: %" PRIu32 ", %" PRIu32 "\n", priv_a.key, priv_a.n);
+    printf("Public: %" PRIu64 ", %" PRIu64 "\n", pub_a.key, pub_a.n);
+    printf("Private: %" PRIu64 ", %" PRIu64 "\n", priv_a.key, priv_a.n);
 
     
     printf("Your keys B: {key}, {n} \n");
-    printf("Public: %" PRIu32 ", %" PRIu32 "\n", pub_b.key, pub_b.n);
-    printf("Private: %" PRIu32 ", %" PRIu32 "\n", priv_b.key, priv_b.n);
+    printf("Public: %" PRIu64 ", %" PRIu64 "\n", pub_b.key, pub_b.n);
+    printf("Private: %" PRIu64 ", %" PRIu64 "\n", priv_b.key, priv_b.n);
 
     printf("I have only pub_a, priv_a, pub_b.\n I'm looking for priv_b...\n");
 
-    uint32_t found_key = find_priv(priv_a, pub_a, pub_b);
-    printf("I've found: %" PRIu32, found_key);
+    uint64_t found_key = find_priv(priv_a, pub_a, pub_b);
+    printf("I've found: %" PRIu64, found_key);
 
     if(found_key == priv_b.key) {
 	    printf(" - it's correct!\n");
