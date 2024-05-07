941e1b4f11ad4c446ef7a38ff2a45593cf3f948a
emo-mruczek <krokcia1@gmail.com>
2024-05-07 14:34:47 +0200

    bezpieka 4: it works

--- bezpieka/4/kod/main.c
+++ bezpieka/4/kod/main.c
@@ -64,7 +64,6 @@ uint64_t find_priv(struct key_pair priv_a, struct key_pair pub_a, uint64_t *p_in
         //printf("k: %" PRIu64 "kphi: %" PRIu64 "/n", k, kphi );
 		while(k < kphi) { //tu tez nie, bo zawsze k = kphi, czemu?
 			x = power(a, k, n);
-            printf("dupa\n");
             
 			if(x != 1 && x != (n-1) && power(x, 2, n) == 1) {
 					p = gcdExtended(x-1, n, &temp1, &temp2);
@@ -76,6 +75,37 @@ uint64_t find_priv(struct key_pair priv_a, struct key_pair pub_a, uint64_t *p_in
 		q = n / p;
         *p_in = p; 
 		return q;
+}
+
+
+uint32_t euclide_test(uint32_t a, uint32_t b) {
+    int16_t s0 = 1, s1 = 0, t0 = 0, t1 = 1, r0 = a, r1 = b;
+
+    while (r1 != 0) {
+        int16_t q = r0 / r1;
+
+        int16_t temp = r1;
+        r1 = r0 - q * r1;
+        r0 = temp;
+
+        temp = s1;
+        s1 = s0 - q * s1;
+        s0 = temp;
+
+        temp = t1;
+        t1 = t0 - q * t1;
+        t0 = temp;
+    }
+
+    return (s0 < 0) ? s0 + b : s0;
+}
+
+
+uint64_t crack(uint64_t p, uint64_t q, uint64_t e) {
+    uint64_t n = p * q;
+    uint64_t euler = (p - 1) * (q - 1);
+    uint64_t d = euclide_test(e, euler);
+    return d;
 }
 
 int main() {
@@ -104,7 +134,9 @@ int main() {
     uint64_t p, q;
     q = find_priv(priv_a, pub_a, &p);
     printf("I've found: %" PRIu64 " as p and %" PRIu64 " as q\n", p, q);
-
+    
+    uint64_t dupa = crack(p, q, pub_b.key);
+    printf("DUPA: %" PRIu64 "\n", dupa);
   /*  if(found_key == priv_b.key) {
 	    printf(" - it's correct!\n");
 	} else {
