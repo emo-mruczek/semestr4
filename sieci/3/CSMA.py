99a8efb0a10f1855b418c15ca09fe704acb9958e
emo-mruczek <krokcia1@gmail.com>
2024-05-10 16:39:15 +0200

    sieci 4: 2 init

---
+++ sieci/3/CSMA.py
@@ -0,0 +1,15 @@
+
+
+class Network:
+
+    def __init__(self, len = 50):
+        self.len = len
+        self.channel = 
+
+
+def main():
+    ch = Network()
+
+
+if __name__ == "__main__":
+    main()
