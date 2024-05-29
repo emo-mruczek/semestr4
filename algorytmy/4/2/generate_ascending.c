f126f0a7ee813048d5057df24910ec07c7d0002f
emo-mruczek <krokcia1@gmail.com>
2024-05-29 17:03:43 +0200

    cd

--- algorytmy/4/2/generate_ascending.c
+++ algorytmy/4/2/generate_ascending.c
@@ -3,13 +3,11 @@
 #include <sys/random.h>
 #include <time.h>
 
-void shuffle(int *array, size_t n)
-{
-    if (n > 1)
-    {
+void shuffle(int *array, size_t n) {
+    if (n > 1) {
         size_t i;
-        for (i = 0; i < n - 1; i++)
-        {
+
+        for (i = 0; i < n - 1; i++) {
             size_t j = i + rand() / (RAND_MAX / (n - i) + 1);
             int t = array[j];
             array[j] = array[i];
